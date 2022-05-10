fa37180251395d209bb2e8eaf4c129ee4ebc68fe
Aidar <Nazarov.Aidar20@yandex.ru>
2022-05-10 22:02:46 +0300

    Aidar commit

--- SteamParsingBot-master/handlers/users/client.py
+++ SteamParsingBot-master/handlers/users/client.py
@@ -3,6 +3,7 @@ from aiogram import types
 from aiogram.dispatcher import FSMContext
 
 from loader import dp
+import aiohttp
 
 from keyboards.inline import ConfirmData
 from keyboards.inline import Choice_country
@@ -18,11 +19,14 @@ async def add_item(call: types.CallbackQuery, state: FSMContext):
 
 @dp.message_handler(state=New_Item.New_item)
 async def get_link(message: types.Message, state: FSMContext):
-    link = message.text.split('/')
-    item = link[-1].partition('?')[0]
-    await state.update_data(Предмет=item)
-    await New_Item.next()
-    await message.answer('Введите цену')
+    if await url_validation(message.text) != 404 and "steamcommunity.com/market/listings/570" in message.text:
+        link = message.text.split('/')
+        item = link[-1].partition('?')[0]
+        await state.update_data(Предмет=item)
+        await New_Item.next()
+        await message.answer('Введите цену')
+    else:
+        await message.answer('Некорректная ссылка\nПовторите ввод')
 
 
 @dp.message_handler(state=New_Item.New_item_price)
@@ -32,7 +36,7 @@ async def get_price(message: types.Message, state: FSMContext):
             await message.answer('Цена должна быть больше 0!\nПовторите ввод')
         else:
             price = float(message.text)
-            await state.update_data(Price=price)
+            await state.update_data(Цена=price)
             await New_Item.next()
             await message.answer('Выберите страну', reply_markup=Choice_country.choice_country)
     else:
@@ -42,9 +46,10 @@ async def get_price(message: types.Message, state: FSMContext):
 @dp.callback_query_handler(Choice_country.Country.filter(), state=New_Item.Country)
 async def to_sho_country(call: types.CallbackQuery, state: FSMContext, callback_data: dict):
     strana = callback_data['country']
-    await state.update_data(Сountry=strana)
+    await state.update_data(Страна=strana)
     card_data = await state.get_data()
-    await call.message.answer(card_data, reply_markup=ConfirmData.confirm_data)
+    await call.message.answer(f"Предмет: {card_data['Предмет']}\nЦена: {card_data['Цена']}\nСтрана: {card_data['Страна']}",
+                              reply_markup=ConfirmData.confirm_data)
     logging.info(card_data)
     await New_Item.next()
 
@@ -60,3 +65,13 @@ async def confirm(call: types.CallbackQuery, state: FSMContext):
 async def confirm(call: types.CallbackQuery, state: FSMContext):
     await New_Item.New_item.set()
     await call.message.answer('Send link')
+
+
+async def url_validation(URL):
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url=URL) as response:
+                await session.close()
+                return response.status
+    except Exception as err:
+        return 404
